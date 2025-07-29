6dbd65a15fece146611ba9fb94b469f33b3d9328
Harini-456 <harinisarav456@gmail.com>
2025-07-29 14:12:19 +0530

    Add files via upload

---
+++ E0223013 functions.R
@@ -0,0 +1,30 @@
+rnorm(5)
+
+rnorm(n=5,sd=8,mean=10)
+
+rnorm(n=5,sd=8,mean=100)
+seq(10,20,3)
+seq(from=10, to=20, length.out=100)
+
+x<- c("a","b","c")
+seq(from =10,to=20, along.with=x)
+
+rep(5,10)
+
+rep(5:6,10)
+
+rep(5:6,each=10)
+
+x<- c("a","b","c")
+rep(x,each=5)
+
+
+x<- c("a","b","c")
+rep(x,times=5)
+
+A = seq(from =10,to=20, along.with=x) 
+A
+sqrt(A)
+
+B <-sqrt(A)
+B
